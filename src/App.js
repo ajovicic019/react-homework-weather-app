64b2811e7696305265739c1b8ec235d2629e297d
ajovicic019 <ajovicic019@gmail.com>
2023-05-23 09:31:21 +0200

    Imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a href="/" target="_blank" rel="noreferrer">
-          {" "}
-          Andrea Jovicic
-        </a>{" "}
-        it is{" "}
-        <a href="/" target="_blank" rel="noreferrer">
-          {" "}
-          open sourced{" "}
-        </a>{" "}
-        and hosted on{" "}
-        <a href="/" target="_blank" rel="noreferrer">
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/andrea-jovicic-880b3b24a/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Andrea Jovicic
+          </a>{" "}
+          it is{" "}
+          <a
+            href="https://github.com/ajovicic019/react-homework-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced{" "}
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://remarkable-raindrop-1f4109.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
