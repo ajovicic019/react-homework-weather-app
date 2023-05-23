4b014b28d6249827221abd83eadebfaf8ccffda0
ajovicic019 <ajovicic019@gmail.com>
2023-05-23 09:24:47 +0200

    Cleaned HTML, CSS, added footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <footer>
+        This project was coded by{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          {" "}
+          Andrea Jovicic
+        </a>{" "}
+        it is{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          {" "}
+          open sourced{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          Netlify
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
